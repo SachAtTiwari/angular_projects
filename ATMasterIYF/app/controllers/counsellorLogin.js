7d5a12a198ae2e48c6a39940c2a155a82b5c6a2a
sachin <sachin.tiwari50@gmail.com>
2018-08-07 18:58:51 +0530

    counsellor login

---
+++ ATMasterIYF/app/controllers/counsellorLogin.js
@@ -0,0 +1,70 @@
+const assert = require('assert');
+var mongo = require('mongodb');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+
+exports.counLogin = function(req, res, next) {
+    try{
+    let db = req.app.locals.db;
+   // console.log('in cou login', req.body.body);
+    db.collection("clogin").find(
+     {_username:req.body.body.username})
+    .toArray(function(err, dvData) {
+        if (err) {
+         console.log("err is ", err);
+         res.send({result:"notok"});
+        }else{
+          /*if login verified returned counsellor devotees */
+          if(dvData.length > 0 && 
+            bcrypt.compareSync(req.body.body.password, dvData[0]._password)){
+             // console.log('passed matched');
+              db.collection("devotees").find(
+                {counsellor: findCounsellor(req.body.body.username)})
+              .toArray(function(err, cdata){
+               // console.log('cdata is', cdata);
+                let token = jwt.sign({user:dvData}, crypto.randomBytes(256),
+                  {expiresIn:900});
+                  res.status(200).json({
+                    result:"ok",
+                    message:"Logged in Successfully",
+                    token:token,
+                    userId: dvData._id,
+                    resources: cdata
+                  })
+              })
+              
+           }else{
+              res.send({result:"notok"});
+           }
+        }
+      })
+    }catch(err){
+      console.log("Exception :", err);
+    }
+  }
+
+  // Return counsellor name 
+  function findCounsellor(username) {
+      console.log('username found', username);
+      cslr = '';
+      switch(username) {
+        case 'sgp':
+          cslr = 'HG Shyam Gopal Prabhuji';
+          break;
+        case 'kvp':
+          cslr = 'HG Kalpvraksha Prabhuji';
+          break;
+        case 'vcp':
+          cslr = 'HG Vaidant Chaitnya Prabhuji';
+          break;
+        case 'pvnp':
+          cslr = 'HG Pundrik Vidhyanidhi Prabhuji';
+          break;
+        case 'jnp':
+          cslr = 'HG Jagadanand Pandit Prabhuji';
+          break;
+      }
+      return cslr;
+  }
