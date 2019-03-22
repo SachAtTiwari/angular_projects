92fafb967eac13a46770fe56c933fc4d6d0fcbd2
sachin <sachin.tiwari50@gmail.com>
2019-03-22 15:36:51 +0530

    Report for VL2 VL3

--- ATMasterIYF/src/app/downloads/downloads.component.ts
+++ ATMasterIYF/src/app/downloads/downloads.component.ts
@@ -3,12 +3,13 @@ import { NgForm } from '@angular/forms';
 import { UserService} from '../devotee.service';
 import { utils, write, WorkBook } from 'xlsx';
 import { saveAs } from 'file-saver';
-import { takeWhile } from 'rxjs/operators';
+import { takeWhile, count } from 'rxjs/operators';
 import { mergeAnalyzedFiles } from '@angular/compiler';
 import {AppComponent} from '../app.component';
 declare var jquery: any;
 declare var $: any;
 import {ViewEncapsulation} from '@angular/core';
+import { Response } from '@angular/http';
 
 
 @Component({
@@ -74,6 +75,41 @@ export class DownloadsComponent implements OnInit {
         }
   }
 
+  excelGeneratorCustom = (d1, d2, result_json, absenteeData, reportData) => {
+    const ws_name = 'Attendance';
+    const wb: WorkBook = { SheetNames: [], Sheets: {} };
+    const ws: any = utils.json_to_sheet(result_json);
+    wb.SheetNames.push(ws_name);
+    wb['!autofilter'] = { ref: 'C4' };
+    wb.Sheets[ws_name] = ws;
+
+
+    const ws_name1 = 'no_presence';
+    const ws1: any = utils.json_to_sheet(absenteeData);
+    wb.SheetNames.push(ws_name1);
+    wb.Sheets[ws_name1] = ws1;
+
+
+    const ws_name2 = 'Final_report';
+    const ws2: any = utils.json_to_sheet(reportData);
+    wb.SheetNames.push(ws_name2);
+    wb.Sheets[ws_name2] = ws2;
+
+
+    const wbout = write(wb, { bookType: 'xlsx', bookSST: true, type: 'binary' });
+    function s2ab(s) {
+      const buf = new ArrayBuffer(s.length);
+      const view = new Uint8Array(buf);
+      for (let i = 0; i !== s.length; ++i) {
+        view[i] = s.charCodeAt(i) & 0xFF;
+      }
+      return buf;
+    }
+    saveAs(new Blob([s2ab(wbout)], { type: 'application/octet-stream' }),
+        d1 + '_' + d2  + '.xlsx');
+
+  }
+
   excelGenerator = (d1, d2, result_json) => {
     const ws_name = 'Attendance';
     const wb: WorkBook = { SheetNames: [], Sheets: {} };
@@ -93,6 +129,31 @@ export class DownloadsComponent implements OnInit {
     saveAs(new Blob([s2ab(wbout)], { type: 'application/octet-stream' }),
         d1 + '_' + d2  + '.xlsx');
 
+  }
+
+  checkHowManyDevoteePresntForGivenDate = (date, myArray) => {
+    let count = 0;
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i].date === date) {
+            count += 1 ;
+        }
+    }
+    return count;
+  }
+
+  generateReport = (date, userData) => {
+    let count = 0;
+    for (let  i = 0; i < userData.result.length; i++) {
+      if ( userData.result[i].attendance !== undefined ) {
+        for (let j = 0; j < userData.result[i].attendance.length; j++) {
+           if (userData.result[i].attendance[j].date === date) {
+               count += 1;
+               break;
+           }
+        }
+      }
+    }
+    return count;
   }
 
   checkIfDevoteePresntForGivenDate = (date, myArray) => {
@@ -133,8 +194,26 @@ export class DownloadsComponent implements OnInit {
       this._userService.downloadToExCounsellor(form.value)
       .subscribe(userData => {
        const result_json = [];
+       const absenteeData = [];
+       const reportData = [];
+       let count = 0;
+       for (let k = 0; k < 4; k++) {
+         const finalReport = {};
+          // day, date, topic, speaker, presence, absence, total
+         count = this.generateReport(classList[k], userData);
+        // console.log('count is ', count, classList[k]);
+         finalReport['Date'] = classList[k];
+         finalReport['Topic'] = 'NA';
+         finalReport['Speaker'] = form.value.counsellor;
+         finalReport['Presence'] = count;
+         finalReport['Absence'] = userData.result.length - count;
+         finalReport['Total'] = userData.result.length;
+         reportData.push(finalReport);
+       }
+      // console.log('report data', reportData);
        for (let i = 0; i < userData.result.length; i++) {
          const objectToInsert = {};
+         const absenteeObject = {};
          objectToInsert['name'] = userData.result[i].name;
          objectToInsert['contact'] = userData.result[i].contact;
          objectToInsert['course'] = userData.result[i].course;
@@ -145,12 +224,17 @@ export class DownloadsComponent implements OnInit {
          if ( userData.result[i].attendance !== undefined ) {
             objectToInsert['classcount'] = userData.result[i].attendance.length;
          }
-         const iterLen = 0;
+
+          // console.log('date, absentee list, presntee count, total, name',
+          //     classList[0], userData.result.length - userData.result[i].attendance.length,
+          //     userData.result[i].attendance.length, userData.result.length,
+          //     userData.result[i].name );
          // get list of last 8 eight classes
          // check if devotee present for that day
          // search for date in attendance array for given counsellor/course
          // if yes add present else absent
-         for (let j = 0; j < 8; j++) {
+         let presentAll = [];
+         for (let j = 0; j < 4; j++) {
           if (classList[j] && userData.result[i].attendance !== undefined) {
             let status = {};
             status = this.checkIfDevoteePresntForGivenDate(
@@ -161,12 +245,26 @@ export class DownloadsComponent implements OnInit {
                 objectToInsert['status'] = 'active';
               }
               objectToInsert[classList[j]] = status['present'];
+            } else {
+              // console.log('user result', userData.result[i].name);
+              presentAll.push('NO');
+              if (presentAll.length === 4) {
+               // console.log('clearing not present any days');
+                presentAll = [];
+                // add in absentdata
+                absenteeObject['name'] = userData.result[i].name;
+                absenteeObject['contact'] = userData.result[i].contact;
+                absenteeObject['course'] = userData.result[i].course;
+                absenteeObject['counsellor'] = userData.result[i].counsellor;
+                absenteeData.push(absenteeObject);
+              }
             }
           }
         }
         result_json.push(objectToInsert);
        }
-       this.excelGenerator(form.value.course, form.value.counsellor, result_json);
+       this.excelGeneratorCustom(form.value.course, form.value.counsellor,
+         result_json, absenteeData, reportData);
       });
      }
   }
@@ -187,8 +285,8 @@ export class DownloadsComponent implements OnInit {
     } else {
       this._userService.getSdlClassCourseCounselor(this.course, this.counsellor)
       .subscribe(sdlClass => {
-        console.log('sdl class', sdlClass);
-       for (let j = 0; j < 8; j++) {
+      //  console.log('sdl class', sdlClass);
+       for (let j = 0; j < 4; j++) {
          if (!sdlClass.result[j]) {
            // console.log('break');
            break;
@@ -197,7 +295,7 @@ export class DownloadsComponent implements OnInit {
            classList.push(sdlClass.result[j].date);
          }
        }
-       console.log('in class list inside', classList, classList.length);
+      // console.log('in class list inside', classList, classList.length);
        this.downloadCounsellorEx(classList, form);
 
       });
